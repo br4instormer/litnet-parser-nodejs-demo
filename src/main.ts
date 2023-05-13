cc6df4393875d2bf0e6c4b615a1b3a024a96d082
br4instormer <br4instormer@gmail.com>
2023-05-13 23:49:13 +0300

    Реализация задачи

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { join } from "node:path";
 import { AsyncResultCallback, QueueObject, queue } from "async";
-import Client from "./client";
 import { AxiosInstance, AxiosRequestConfig } from "axios";
+import Client from "./client";
+import Parser from "./parser";
 import { QueueParams } from "./types";
+import CSVStorage from "./storage/csv-storage";
+import { IStorage } from "./storage/storage-interface";
 
 const SIMULTANEOUS_DOWNLOADS = 10;
+const STORAGE_FILENAME = "storage.csv";
+const STORAGE_FILEPATH = join(__dirname, STORAGE_FILENAME);
 const axiosConfig: AxiosRequestConfig = {
   baseURL: "https://litnet.com/ru",
   responseType: "document",
@@ -27,35 +33,66 @@ const downloadList = async (
   cb: Function
 ): Promise<string> => cb((await client.request(options)).data);
 
+const storage = new CSVStorage(STORAGE_FILEPATH);
 const client = Client.create(axiosConfig);
 Client.setRetry(client, axiosRetryOptions);
 
 const q = queue<QueueParams>(downloadList, SIMULTANEOUS_DOWNLOADS);
-const cb = async <T>(body: T): Promise<void> => console.log(body);
+const parseList = async <T>(body: T): Promise<void> => {
+  if (!body) {
+    return;
+  }
+
+  const books = Parser.books(`${body}`);
+
+  storage.append(books);
+};
+const parsePagesNum = async (
+  client: AxiosInstance,
+  options: AxiosRequestConfig
+) => {
+  const body = await downloadList(
+    { client, options },
+    (body: string): string => body
+  );
+
+  return Parser.pageNumber(body);
+};
 
 async function main(
   client: AxiosInstance,
   q: QueueObject<QueueParams>,
-  cb: AsyncResultCallback<string>
+  cb: AsyncResultCallback<string>,
+  storage: IStorage
 ): Promise<void> {
-  for (let page = 1; page <= 1; page++) {
-    const options = {
-      url: "/top/all",
-      params: {
-        alias: "all",
-        sort: "latest",
-        page,
-      },
-    };
+  const options = {
+    url: "/top/all",
+    params: {
+      alias: "all",
+      sort: "latest",
+      page: 1,
+    },
+  };
+  const pages = await parsePagesNum(client, options);
 
+  for (let page = 1; page <= pages; page++) {
     q.push<string>(
       {
         client,
-        options,
+        options: {
+          ...options,
+          params: {
+            ...options.params,
+            page,
+          },
+        },
       },
       cb
     );
   }
+
+  await q.drain();
+  await storage.store();
 }
 
-main(client, q, cb);
+main(client, q, parseList, storage);
